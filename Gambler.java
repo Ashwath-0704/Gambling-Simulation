f69dca16175889299be39f5575598abec5f64c47
Achyuth Naidu <ashwath.bly@gamil.com>
2021-12-11 18:33:49 +0530

    UC2Gambler

--- Gambler.java
+++ Gambler.java
@@ -1,4 +1,5 @@
-import java.util.*;
+mport java.util.*;
+import  java.util.Random;
 
 public class Gambler {
     public static void main(String[] args) {
@@ -11,5 +12,18 @@ public class Gambler {
     System.out.print("Enter the bet : ");  
     k=sc.nextInt();
     int bets = k;
+
+    int upperbound = 2;
+    Random rand = new Random();
+    int random = rand.nextInt(upperbound);// 0 1
+
+    if (random==0) {
+        stake--;    // decrement s--
+        System.out.print("The current stake is :"+stake);  //150
+    } else if (random==1){
+        stake++;   // increment s++ 
+        System.out.print("The current stake is :"+stake);  //150
+    }
+
     }
 }
