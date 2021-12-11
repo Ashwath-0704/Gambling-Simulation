ae83cb9aafcdce9b12e4b018eb59ef4560746331
Achyuth Naidu <ashwath.bly@gamil.com>
2021-12-11 18:06:25 +0530

    UC1Gambler

---
+++ Gambler.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Gambler {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in); 
+    int n;
+    System.out.print("Enter the stake you want : ");  
+    n=sc.nextInt(); 
+    int stake  = n;
+    int k;
+    System.out.print("Enter the bet : ");  
+    k=sc.nextInt();
+    int bets = k;
+    }
+}
